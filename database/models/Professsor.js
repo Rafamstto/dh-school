bed1d9fec19290a316418139439b72302993fa22
rafaelmstto <rafaelmstto@mail.comm>
2022-03-23 22:01:39 -0300

    Aula 23-03

---
+++ database/models/Professsor.js
@@ -0,0 +1,30 @@
+module.exports = (sequelize, DataTypes) => {
+    const Professor = sequelize.define("Professor", {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
+        },
+        nome: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        sobrenome: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    }, {
+        tableName: "professores",
+        timestamps: false
+    });
+
+    Professor.associate = (models) => {
+        Professor.hasMany(models.Turma, {
+            foreignKey: "professores_id",
+            as: "turmas"
+        })
+    };
+
+    return Professor;
+}
